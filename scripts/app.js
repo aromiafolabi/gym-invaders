7963c4e2aea438ac02cbb220c7ceb9a364cf133b
Aromi Afolabi <aromiafolabi@googlemail.com>
2021-11-14 18:25:56 +0000

    last sprite on board but not moving properly yet

--- scripts/app.js
+++ scripts/app.js
@@ -18,6 +18,7 @@
 const grid = document.querySelector('.grid')
 const cells = []
 const startButton = document.querySelector('#start')
+// let dumbbellPosition = document.querySelector('.grid > div')
 
 // Grid Variables
 const width = 10
@@ -25,7 +26,9 @@ const gridCellCount = width * width
 
 // Game Variables
 let burgerPosition = 95
-// let dumbbellPosition = 0
+let dumbbellPosition = [0,1,2,3,4,5,6,7,8,9]
+let playerLaser = burgerPosition - width
+
 
 
 // Building the grid
@@ -48,31 +51,37 @@ function addBurger(){
   cells[burgerPosition].classList.add('burger')
 }
 
-// function removeDumbbell(){
-//   cells[0].classList.remove('dumbell')
-//   cells[1].classList.remove('dumbell')
-//   cells[2].classList.remove('dumbell')
-//   cells[3].classList.remove('dumbell')
-//   cells[4].classList.remove('dumbell')
-//   cells[5].classList.remove('dumbell')
-//   cells[6].classList.remove('dumbell')
-//   cells[7].classList.remove('dumbell')
-// }
+function addPlayerLaser(){
+  cells[playerLaser].classList.add('playerLaser')
+  
+}
 
-// function addDumbbell(){
-//   cells[0].classList.add('dumbbell')
-//   cells[1].classList.add('dumbbell')
-//   cells[2].classList.add('dumbbell')
-//   cells[3].classList.add('dumbbell')
-//   cells[4].classList.add('dumbbell')
-//   cells[5].classList.add('dumbbell')
-//   cells[6].classList.add('dumbbell')
-//   cells[7].classList.add('dumbbell')
-// }
+function removePlayerLaser(){
+  cells[playerLaser].classList.remove('playerLaser')
+
+}
+
+function removeDumbbell(){
+  dumbbellPosition.forEach((index) => {
+    cells[index].classList.remove('dumbbell')
+  })
+  
+}
+
+
+function addDumbbell(){
+  dumbbellPosition.forEach((index) => {
+    cells[index].classList.add('dumbbell') 
+  })
+  
+}
+
 function handleGameStart(){
   window.setInterval(() => {
     addBurger()
-    // handleComputerControls()
+    addDumbbell()
+    removePlayerLaser()
+    addPlayerLaser()
   }, 500)
 }
 function handlePlayerControls(e){
@@ -80,23 +89,50 @@ function handlePlayerControls(e){
   console.log(x)
   
   removeBurger()
+  removePlayerLaser()
+
   if (e.code === 'ArrowLeft' && x > 0) {
     return burgerPosition-- 
   } else if (e.code === 'ArrowRight' && x < width - 1) {
     return burgerPosition++
+  } else if (e.code === 'space') {
+    return playerLaser
   }  else {
-    return 'invald do nothing'
-  }
+    return 'invalid do nothing'
+  } 
 }
 
+
 // function handleComputerControls(){
+//   removeDumbbell()
+//   // dumbbellPosition.forEach((index) => {
+//   //   dumbbellPosition = [index]
+//   //   const y = dumbbellPosition % length
+//   //   console.log(y)
+//   //   console.log(dumbbellPosition)
+//   // })
+//   // window.setInterval(() => {
+//   dumbbellPosition.forEach((index) => {
+//     dumbbellPosition = [index]
+//     const y = Math.floor(dumbbellPosition % width)
+//     dumbbellPosition % width
+//     Math.floor(dumbbellPosition % width)
+//     console.log('y', y)
+//     console.log(dumbbellPosition)
+//     if (y > 0) {
+//       return dumbbellPosition -= width
+//     } else if (y < width - 1) {
+//       return dumbbellPosition += width
+//     }
+//     // }, 1000)
+    
+//   })
 //   addDumbbell()
-//   window.setInterval(() => {
-//     dumbbellPosition += width
-//   }, 1000)
 // }
+// handleComputerControls()
 
 // Events
 startButton.addEventListener('click', handleGameStart)
 document.addEventListener('keyup', handlePlayerControls)
+// document.addEventListener('mouseenter', handleComputerControls)
 
